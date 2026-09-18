aff6076e008188909a9b8e4d51eea7336766e69d
hillaryalvespontes-hash <hillary.alves.pontes@escola.pr.gov.br>
2026-09-18 08:46:07 -0300

    alterei inex e  script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const botoesCurtir= document.querySelectorAll (".curtir");
+    botoesCurtir .forEach (function(botaoCurtir){
+        let curtiu= false;
+        botaoCurtir.addEventLitener("click",curtir);
+function curtiu(){
+    const contador = botaoCurtir .querySelector ("")
+}
+    }
